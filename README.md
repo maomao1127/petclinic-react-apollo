cbb1de4788597ca6fa0807389666a595155d12de
宋丽 <songli12@taikanglife.com>
2018-06-06 09:59:11 +0800

    --优化

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # petclinic-react-apollo
 学习使用react-apollo
+
+工程由create-react-app创建，主题使用antd
+
+用到的后台数据由https://github.com/spring-petclinic/spring-petclinic-graphql的backend部分提供
--- src/index.js
+++ src/index.js
@@ -6,31 +6,12 @@ import App from "./App"
 import ApolloClient from "apollo-boost"
 import {ApolloProvider} from 'react-apollo'
 import {BrowserRouter, Route} from "react-router-dom";
-// import {message} from "antd"
-import {BatchHttpLink} from "apollo-link-batch-http"
-import {InMemoryCache} from "apollo-cache-inmemory"
+import {message} from "antd"
 
 //TODO:没有生效，还是三个请求
-const link = new BatchHttpLink({url: "http://localhost:9977/graphql"});
-
-const cache = new InMemoryCache({
-  cacheRedirects: {
-    Query: {
-      owner: (_, {id}, {getCacheKey}) =>
-          getCacheKey({__typename: 'Owner', id}),
-      pet: (_, {id}, {getCacheKey}) =>
-          getCacheKey({__typename: 'Pet', id})
-    }
-  }
-});
-
-const client = new ApolloClient({
-  link: link,
-  cache: cache
-});
-
-// const client = new ApolloClient({
-//   url: "http://localhost:9977/graphql",
+// const link = new BatchHttpLink({url: "http://localhost:9977/graphql"});
+//
+// const cache = new InMemoryCache({
 //   cacheRedirects: {
 //     Query: {
 //       owner: (_, {id}, {getCacheKey}) =>
@@ -38,16 +19,33 @@ const client = new ApolloClient({
 //       pet: (_, {id}, {getCacheKey}) =>
 //           getCacheKey({__typename: 'Pet', id})
 //     }
-//   },
-//   onError: ({graphQLErrors, networkError}) => {
-//     if (graphQLErrors) {
-//       message.error(graphQLErrors[0].message, 1);
-//     }
-//     if (networkError) {
-//       message.error(networkError.message, 1);
-//     }
 //   }
 // });
+//
+// const client = new ApolloClient({
+//   link: link,
+//   cache: cache
+// });
+
+const client = new ApolloClient({
+  url: "http://localhost:9977/graphql",
+  cacheRedirects: {
+    Query: {
+      owner: (_, {id}, {getCacheKey}) =>
+          getCacheKey({__typename: 'Owner', id}),
+      pet: (_, {id}, {getCacheKey}) =>
+          getCacheKey({__typename: 'Pet', id})
+    }
+  },
+  onError: ({graphQLErrors, networkError}) => {
+    if (graphQLErrors) {
+      message.error(graphQLErrors[0].message, 1);
+    }
+    if (networkError) {
+      message.error(networkError.message, 1);
+    }
+  }
+});
 
 ReactDOM.render(
     <ApolloProvider client={client}>
